055ec9695a0bf9c30126c0deddfe1abd0b16b4d8
l1keafox <raymond.ed.lewis@gmail.com>
2024-09-26 11:32:30 -0600

    maria too postgres

--- example/index.js
+++ example/index.js
@@ -1,30 +1,26 @@
-const mysql = require('mysql2/promise');
+const { Pool } = require('pg');
+require('dotenv').config({ path: __dirname + '/.env' });
 
 // Create the connection pool
-const pool = mysql.createPool({
-  host: process.env.DOCKER_URL, // If running from a different machine, replace with your Raspberry Pi's IP address
+const pool = new Pool({
+  host: "localhost", // If running from a different machine, replace with your Raspberry Pi's IP address
   user: 'fox',
   password: 'raymond',
-  database: 'sql',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  database: 'my_database',
+  port: 5432, // Default port for PostgreSQL
+  max: 10, // Max number of clients in the pool
+  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
 });
 
 async function testConnection() {
   try {
     // Test the connection by querying the database
-    const [rows] = await pool.query('SELECT 1 + 1 AS solution');
-    console.log('MARIADB Connected! The solution is:', rows[0].solution);
+    const res = await pool.query('SELECT 1 + 1 AS solution');
+    console.log('Connected! The solution is:', res.rows[0].solution);
   } catch (err) {
     console.error('Error connecting to the database:', err.message);
   }
 }
 
 testConnection();
-
-const db = require("./connection");
-
-db.once("open", () => {
-  console.log("Mongo DB connection is opened once")
-});
---
+++ example/postgresTest.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+require('dotenv').config({ path: __dirname + '/.env' });
+
+// Create the connection pool
+const pool = new Pool({
+  host: "localhost", // If running from a different machine, replace with your Raspberry Pi's IP address
+  user: 'fox',
+  password: 'raymond',
+  database: 'my_database',
+  port: 5432, // Default port for PostgreSQL
+  max: 10, // Max number of clients in the pool
+  idleTimeoutMillis: 30000, // Close idle clients after 30 seconds
+  connectionTimeoutMillis: 2000, // Return an error after 2 seconds if connection could not be established
+});
+
+async function testConnection() {
+  try {
+    // Test the connection by querying the database
+    const res = await pool.query('SELECT 1 + 1 AS solution');
+    console.log('Connected! The solution is:', res.rows[0].solution);
+  } catch (err) {
+    console.error('Error connecting to the database:', err.message);
+  }
+}
+
+testConnection();
